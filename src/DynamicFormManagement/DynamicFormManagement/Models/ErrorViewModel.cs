a581c140770a23b245e6bc4b74f83a857095b611
Shariar Alfaz <77987580+Shariar-Alfaz@users.noreply.github.com>
2023-11-03 18:15:05 +0600

    update

--- src/DynamicFormManagement/DynamicFormManagement/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DynamicFormManagement.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
